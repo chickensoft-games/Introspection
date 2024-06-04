4be5196234cd73fbd061a094e97f064ff2e7c5e2
Joanna May <joannamadisonmay@gmail.com>
2024-06-03 19:30:00 -0500

    feat: initial commit

---
+++ Chickensoft.Introspection/src/Package.cs
@@ -0,0 +1,11 @@
+namespace Chickensoft.Introspection;
+
+/// <summary>
+/// Package class.
+/// </summary>
+public class Package {
+  /// <summary>
+  /// Method.
+  /// </summary>
+  public string Method() => "Hello, world!";
+}
